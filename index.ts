a8f0ba62fcecc08a92637b75bf761bec22f04fba
Habibmanzoor <159249434+Habibmanzoor@users.noreply.github.com>
2024-03-04 09:17:54 -0800

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,34 @@
+//var message : string = "habib!"
+//console.log(message);
+//message = "123"
+//console.log(message);
+//message = "1981"
+//console.log(message);
+
+// E.g Good Day, Ahmad!
+
+//var namE : string ="habib";
+//console.log(namE);
+
+
+//let personAge : number = 30;
+//console.log(personAge);
+
+//let finalAge :number =personAge +5;
+//console.log(finalAge);
+
+//string 
+//let Class : string ="habib";
+//console.log(Class);
+
+//number
+//let books :number = 50;
+//console.log(books);
+
+//bolean => true/false
+//let A : boolean =true
+//console.log(A);
+
+//null
+let A : null =null
+console.log(A);
